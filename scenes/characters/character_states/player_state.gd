e2f110dbb9f20f4c859e38cdf5ef12dbd481d15f
Michael Crozier <mac5609@gmail.com>
2025-06-09 22:02:26 -0600

    add AI onduty steering force

--- scenes/characters/character_states/player_state.gd
+++ scenes/characters/character_states/player_state.gd
@@ -3,6 +3,7 @@ extends Node
 
 signal state_transition_requested(new_state: Player.State, state_data: PlayerStateData)
 
+var ai_behavior : AIBehavior = null
 var animation_player : AnimationPlayer = null
 var ball : Ball = null
 var player : Player = null
@@ -14,7 +15,7 @@ var target_goal : Goal = null
 
 func setup(context_player: Player, context_data : PlayerStateData, context_animation_player: AnimationPlayer,
 		   context_ball: Ball, context_teammate_detection_area: Area2D, context_ball_detection_area: Area2D,
-		   context_own_goal: Goal, context_target_goal: Goal) -> void:
+		   context_own_goal: Goal, context_target_goal: Goal, context_ai_behavior: AIBehavior) -> void:
 	player = context_player
 	animation_player = context_animation_player
 	state_data = context_data
@@ -23,6 +24,7 @@ func setup(context_player: Player, context_data : PlayerStateData, context_anima
 	ball_detection_area = context_ball_detection_area
 	own_goal = context_own_goal
 	target_goal = context_target_goal
+	ai_behavior = context_ai_behavior
 
 func transition_state(new_state: Player.State, new_state_data: PlayerStateData = PlayerStateData.new()) -> void:
 		state_transition_requested.emit(new_state, new_state_data)
